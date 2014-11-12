ac1955d980d591d3d141a5235d50576a627bd1e4
Gustavo Nicolas Ogawa <ogawa.gustavo.n@gmail.com>
2014-11-12 10:10:42 -0300

    Create setup.py by Eric Balderrama

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='FBHT',
+      version='3.0',
+      description='FBHT',
+      author='Chino Ogawa',
+      url='https://github.com/chinoogawa/fbht',
+	  console=['main.py']      
+     )
